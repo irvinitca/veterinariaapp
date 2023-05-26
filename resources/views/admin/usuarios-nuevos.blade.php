1accf7a162aa9d233bc107032091eb66c896b303
Napoleón Sáenz <napoleonsaenz85@gmail.com>
2023-05-26 13:56:25 -0600

    update-formUsers

--- resources/views/admin/usuarios-nuevos.blade.php
+++ resources/views/admin/usuarios-nuevos.blade.php
@@ -8,7 +8,7 @@
     <div class="signup-container">
         <div class="left-container">
           <h1>
-            <i class="fas fa-paw"></i>
+            <img class="logovet" src="{{ asset('logo/cio-logo.png') }}" alt="Logo de CIO">
             Veterinaria CIO
           </h1>
           <div class="puppy">
@@ -27,23 +27,44 @@
 
                     <div class="form-group">
                         <label for="name">Nombre:</label>
-                        <input name="name" id="name" class="form-control" rows="3" required value="{{ old('name') }}">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input name="name" id="name" class="form-control" rows="3" required value="{{ old('name') }}">
+                        </div>
                     </div>
 
                     <div class="form-group">
                         <label for="email">Correo Electrónico:</label>
-                        <input name="email" id="email" class="form-control" rows="3" required value="{{ old('email') }}">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                            </div>
+                            <input name="email" id="email" class="form-control" rows="3" required value="{{ old('email') }}">
+                        </div>
                     </div>
 
                     <div class="form-group">
                         <label for="password">Contraseña:</label>
-                        <input name="password" id="password" type="password" class="form-control" rows="3" required>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            </div>
+                            <input name="password" id="password" type="password" class="form-control" rows="3" required>
+                        </div>
                     </div>
 
                     <div class="form-group">
                         <label for="password_confirmation">Confirmar Contraseña:</label>
-                        <input name="password_confirmation" id="password_confirmation" type="password" class="form-control" rows="3" required>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            </div>
+                            <input name="password_confirmation" id="password_confirmation" type="password" class="form-control" rows="3" required>
+                        </div>
                     </div>
+
                     @if ($errors->has('password') || $errors->has('password_confirmation'))
                         <div class="alert alert-danger">
                             <ul>
@@ -56,58 +77,29 @@
                             </ul>
                         </div>
                     @endif
+
                     <div class="form-group">
-                        <label for="role_id"></label>
+                        <label for="role_id">Tipo de Usuario:</label>
                         <select name="role_id" id="role_id" class="form-control select2">
-                            <option value="" disabled selected>Seleccionar tipo de Usuario</option>
+                            <option value="" disabled selected>Seleccione</option>
                             @foreach ($roles as $role)
                                 <option value="{{ $role->id }}" {{ old('role_id') == $role->id ? 'selected' : '' }}>{{ $role->name }}</option>
                             @endforeach
                         </select>
                     </div>
 
-
                     <div class="setfooter">
-                        <button id="back">Cancelar</button>
-                        <button id="next" type="submit" class="btn btn-primary">Guardar</button>
+                        <button id="back" type="button">Cancelar</button>
+                        <button id="next" type="submit" class="btn">Guardar</button>
                     </div>
                 </form>
-
-             {{--   <form action="{{ route('users.store') }}" method="POST">
-                @csrf
-
-                <div class="form-group">
-                    <label for="name">Nombre:</label>
-                    <input name="name" id="name" class="form-control" rows="3" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Correo Electrónico:</label>
-                    <input name="email" id="email" class="form-control" rows="3" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="password">password:</label>
-                    <input name="password" id="password" class="form-control" rows="3" required>
-                </div>
-
-                <div class="form-group">
-                    <label  for="role_id">Rol de Usuario:</label>
-                    <select name="role_id" class="form-control select2">
-                        @foreach ($roles as $role)
-                            <option value="{{ $role->id }}">{{ $role->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-
-                <div class="setfooter">
-                  <button id="back">Cancelar</button>
-                  <button id="next" type="submit" class="btn btn-primary">Guardar</button>
-                </div>
-            </form>--}}
             </div>
 
+            <script>
+                document.getElementById("back").addEventListener("click", function() {
+                    window.location.href = "/dashboard";
+                });
+            </script>
         </div>
       </div>
 
