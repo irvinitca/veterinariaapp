a112577dcb7286c7ee24df045da83e26145f605c
irvinitca <irvin.paredes@itca.edu.sv>
2023-06-16 13:19:42 -0600

    form route

--- resources/views/owner/owners-editar.blade.php
+++ resources/views/owner/owners-editar.blade.php
@@ -26,7 +26,7 @@
             </header>
 
             <div class="formdiv">
-                <form action="/owners/{{ $owner->id }}" method="POST" onsubmit="return confirmarGuardar(event)">
+                <form action="{{ config('app.base_url') }}owners/{{ $owner->id }}" method="POST" onsubmit="return confirmarGuardar(event)">
                     @csrf
                     @method('PUT')
                     <div class="form-group">
