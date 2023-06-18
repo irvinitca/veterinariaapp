6a6fccfd0c0ef668041201856be0ccaf9af8cbd2
Napoleón Sáenz <napoleonsaenz85@gmail.com>
2023-06-18 16:27:42 -0600

    required selects

--- resources/views/pet/pets-editar.blade.php
+++ resources/views/pet/pets-editar.blade.php
@@ -45,7 +45,7 @@
                         <div>
                             <div class="form-group">
                                 <label for="type">Tipo:</label>
-                                <select wire:model="selectedType" name="selectedType" id="selectedType" class="form-control">
+                                <select wire:model="selectedType" name="selectedType" id="selectedType" class="form-control" required>
                                     <option value="">Selecciona el tipo</option>
                                     @foreach ($types as $type)
                                     <option value="{{ $type->id }}" {{ $pet->type === $type->name ? 'selected' : '' }}>{{ $type->name }}</option>
@@ -58,7 +58,7 @@
                         <div>
                             <div class="form-group">
                                 <label for="breed">Razas:</label>
-                                <select wire:model="selectedBreed" name="selectedBreed" id="selectedBreed" class="form-control select2">
+                                <select wire:model="selectedBreed" name="selectedBreed" id="selectedBreed" class="form-control select2" required>
                                     <option value="">Selecciona la raza</option>
                                     @foreach ($breeds as $breed)
                                     <option value="{{ $breed->id }}" {{ $pet->breed === $breed->name ? 'selected' : '' }}>{{ $breed->name }}</option>
